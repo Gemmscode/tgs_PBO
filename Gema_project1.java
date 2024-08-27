652980e737027cce7cf5a1a1feb42df20bb3f22c
Gemmscode <gemari1012@gmail.com>
2024-08-27 12:25:31 +0700

    Tgs_PBO_2

---
+++ Gema_project1.java
@@ -0,0 +1,27 @@
+/*
+
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gema_project1;
+
+/**
+ *
+ * @author Najmi Mukia Barkah
+ */
+public class Gema_project1 {
+
+    public static void main(String[] args) {
+        // Membuat informasi pribadi
+        String Nama = "Gema Rizqi Mulia Akbar";
+        String NPM = "2310506008";
+        String Alamat = "Kota Bekasi";
+        String No_hp = "081212700436";
+        
+        // Menampilkan hasil informasi pribadi
+        System.out.println("Nama   : " + Nama);
+        System.out.println("NPM    : " + NPM);
+        System.out.println("Alamat : " + Alamat);
+        System.out.println("No_HP  : " + No_hp);       
+    }
+}
